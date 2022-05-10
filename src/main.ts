a2be056220f7659487534acb050d1aee0c4b4ec5
Markus Maga <markus@maga.se>
2022-05-11 01:52:02 +0200

    feat: setup git-crypt
    Signed-off-by: Markus Maga <markus@maga.se>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
+import * as os from 'os'
+import * as path from 'path'
 
 async function run(): Promise<void> {
+  const version = core.getInput('version')
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    let toolPath: string = tc.find('git-crypt', version)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // If not found in cache, download
+    if (toolPath) {
+      core.info(`Found in cache @ ${toolPath}`)
+    } else {
+      const destination = path.join(os.homedir(), '.git-crypt')
+      core.info(`Install destination is ${destination}`)
 
-    core.setOutput('time', new Date().toTimeString())
+      const downloaded = await tc.downloadTool(
+        `https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${version}.tar.gz`
+      )
+      const extractedPath = await tc.extractTar(downloaded, destination)
+      const workspace = path.join(extractedPath, `git-crypt-${version}`)
+      core.info(`Extracted ${downloaded} to ${extractedPath}`)
+
+      await exec.getExecOutput('make', ['install', `PREFIX=${extractedPath}`], {
+        cwd: workspace
+      })
+
+      toolPath = await tc.cacheDir(
+        path.join(destination, 'bin'),
+        'git-crypt',
+        version
+      )
+    }
+
+    core.addPath(toolPath)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
