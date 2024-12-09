c4fd10479b92a6abd72e2143999dee151c255921
Markus Maga <markus@maga.se>
2024-12-09 13:41:20 +0100

    fix: use fork
    Signed-off-by: Markus Maga <markus@maga.se>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
+// import * as exec from '@actions/exec'
 import * as tc from '@actions/tool-cache'
 import * as os from 'os'
 import * as path from 'path'
@@ -17,32 +17,34 @@ async function run(): Promise<void> {
       const destination = path.join(os.homedir(), '.git-crypt')
       core.info(`Install destination is ${destination}`)
 
-      const downloaded = await tc.downloadTool(
-        `https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${version}.tar.gz`
-      )
-      const extractedPath = await tc.extractTar(downloaded, destination)
-      const workspace = path.join(extractedPath, `git-crypt-${version}`)
-      core.info(`Extracted ${downloaded} to ${extractedPath}`)
+      // const downloaded = await tc.downloadTool(
+      //   `https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${version}.tar.gz`
+      // )
 
-      let extraArgs: string[] = []
-
-      if (process.env.ImageOS === 'ubuntu22') {
-        extraArgs = [`CXXFLAGS='-DOPENSSL_API_COMPAT=0x30000000L'`]
-      }
-
-      await exec.getExecOutput(
-        'make',
-        ['install', `PREFIX=${extractedPath}`, ...extraArgs],
-        {
-          cwd: workspace
-        }
+      await tc.downloadTool(
+        `https://github.com/maxisam/git-crypt/releases/download/v${version}/git-crypt-${version}-linux-x86_64`,
+        destination
       )
 
-      toolPath = await tc.cacheDir(
-        path.join(destination, 'bin'),
-        'git-crypt',
-        version
-      )
+      // const extractedPath = await tc.extractTar(downloaded, destination)
+      // const workspace = path.join(extractedPath, `git-crypt-${version}`)
+      // core.info(`Extracted ${downloaded} to ${extractedPath}`)
+
+      // let extraArgs: string[] = []
+
+      // if (process.env.ImageOS === 'ubuntu22') {
+      //   extraArgs = [`CXXFLAGS='-DOPENSSL_API_COMPAT=0x30000000L'`]
+      // }
+
+      // await exec.getExecOutput(
+      //   'make',
+      //   ['install', `PREFIX=${extractedPath}`, ...extraArgs],
+      //   {
+      //     cwd: workspace
+      //   }
+      // )
+
+      toolPath = await tc.cacheDir(path.join(destination), 'git-crypt', version)
     }
 
     core.addPath(toolPath)
